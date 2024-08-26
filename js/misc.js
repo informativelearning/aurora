781a9e964c6e77df2c0ab86ceb943cded2010184
Ezra-531 <4josephhernandez12@gmail.com>
2024-08-25 18:55:06 -0700

    Updated Misc to what it should be

--- js/misc.js
+++ js/misc.js
@@ -5,10 +5,12 @@ const output = (msg, clr) => {
   txt.innerText = msg;
 };
 
+
 // Cache DOM elements
 const tcInput = document.getElementById("userinput");
 const urlInput = document.getElementById("url-target");
 const presetSelect = document.getElementById("apply-presets");
+
 
 // Function to change the tab title
 const changeTabTitle = () => {
@@ -22,6 +24,7 @@ const changeTabTitle = () => {
   }
   tcInput.value = "";
 };
+
 
 // Function to change the tab icon
 const changeTabIcon = () => {
@@ -39,17 +42,20 @@ const changeTabIcon = () => {
   tcInput.value = "";
 };
 
+
 // Function to validate URL
 const validURL = (str) => {
   const exp = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
   return !!new RegExp(exp).test(str);
 };
 
+
 // Function to reset tab settings
 const resetTabSettings = () => {
   ["icon", "title"].forEach(item => window.localStorage.removeItem(item));
   window.location.reload();
 };
+
 
 // Function to apply URL and title
 function applyUrl(url, title) {
@@ -60,6 +66,7 @@ function applyUrl(url, title) {
   output("Preset applied successfully!", "green");
 }
 
+
 // Event handler for the 'Create page' button
 document.getElementById("create").onclick = function () {
   if (!urlInput.value.startsWith("https://") && !urlInput.value.startsWith("http://")) {
@@ -67,7 +74,7 @@ document.getElementById("create").onclick = function () {
   } else if (urlInput.value.startsWith("http://")) {
     urlInput.value = `https://${urlInput.value.substring(7)}`;
   }
-  
+ 
   const win = window.open();
   win.document.body.style.margin = "0";
   win.document.body.style.height = "100vh";
@@ -81,11 +88,12 @@ document.getElementById("create").onclick = function () {
   win.document.body.appendChild(iframe);
 };
 
+
 // Event handler for the preset select dropdown
 presetSelect.addEventListener('change', function() {
   switch (this.value) {
     case "canvas":
-      applyUrl('https://img.icons8.com/?size=512&id=oECahFP3ibID&format=png', 'Canvas');
+      applyUrl('https://img.icons8.com/?size=512&id=oECahFP3ibID&format=png', 'Dashboard');
       break;
     case "aeries":
       applyUrl('https://wascouhsd.aeries.net/student/favicon.ico', 'Aeries Student Dashboard');
@@ -98,16 +106,14 @@ presetSelect.addEventListener('change', function() {
   }
 });
 
+
 // Event listeners for the buttons
 document.getElementById('set-title-btn').addEventListener('click', changeTabTitle);
 document.getElementById('set-icon-btn').addEventListener('click', changeTabIcon);
 document.getElementById('reset-btn').addEventListener('click', resetTabSettings);
 document.getElementById('imitate-canvas-btn').addEventListener('click', () => {
-  applyUrl('https://img.icons8.com/?size=512&id=oECahFP3ibID&format=png', 'Canvas');
+  applyUrl('https://du11hjcvx0uqb.cloudfront.net/dist/images/favicon/favicon-32x32.png', 'Canvas');
 });
-document.getElementById('imitate-gmail-btn').addEventListener('click', () => {
-  applyUrl('https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico', 'Gmail');
-});
-document.getElementById('imitate-aeries-btn').addEventListener('click', () => {
-  applyUrl('https://wascouhsd.aeries.net/student/favicon.ico', 'Aeries Student Dashboard');
-});
+
+
+
