cecff53a2979fa1af565b1aac6ebacfb098a7f1d
Ezra-531 <4josephhernandez12@gmail.com>
2024-08-26 19:35:21 -0700

    change misc

--- js/misc.js
+++ js/misc.js
@@ -5,30 +5,27 @@ const output = (msg, clr) => {
   txt.innerText = msg;
 };
 
-
 // Cache DOM elements
 const tcInput = document.getElementById("userinput");
 const urlInput = document.getElementById("url-target");
 const presetSelect = document.getElementById("apply-presets");
 
-
 // Function to change the tab title
 const changeTabTitle = () => {
-  if (tcInput.value === "") {
+  if (tcInput.value.trim() === "") {
     window.localStorage.removeItem("title");
     output("No title entered. Default applied, refresh to see changes", "red");
   } else {
     window.localStorage.setItem("title", tcInput.value);
-    window.document.title = tcInput.value;
+    document.title = tcInput.value;
     output("Title change successful", "green");
   }
   tcInput.value = "";
 };
 
-
 // Function to change the tab icon
 const changeTabIcon = () => {
-  if (tcInput.value === "") {
+  if (tcInput.value.trim() === "") {
     document.querySelector("link[rel*='icon']").href = "";
     window.localStorage.removeItem("icon");
     output("No image entered. Default applied, refresh to see changes", "red");
@@ -42,13 +39,11 @@ const changeTabIcon = () => {
   tcInput.value = "";
 };
 
-
 // Function to validate URL
 const validURL = (str) => {
-  const exp = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-  return !!new RegExp(exp).test(str);
+  const exp = /^(https?:\/\/)?([\w\d-]+\.)+[\w]{2,}(\/[\w\d-]*)*\/?$/i;
+  return exp.test(str);
 };
-
 
 // Function to reset tab settings
 const resetTabSettings = () => {
@@ -56,25 +51,24 @@ const resetTabSettings = () => {
   window.location.reload();
 };
 
-
 // Function to apply URL and title
-function applyUrl(url, title) {
-  document.getElementById("userinput").value = url;
+const applyUrl = (url, title) => {
+  urlInput.value = url;
   changeTabIcon();
-  document.getElementById("userinput").value = title;
+  tcInput.value = title;
   changeTabTitle();
   output("Preset applied successfully!", "green");
-}
-
+};
 
 // Event handler for the 'Create page' button
-document.getElementById("create").onclick = function () {
-  if (!urlInput.value.startsWith("https://") && !urlInput.value.startsWith("http://")) {
-    urlInput.value = `https://${urlInput.value}`;
-  } else if (urlInput.value.startsWith("http://")) {
-    urlInput.value = `https://${urlInput.value.substring(7)}`;
+document.getElementById("create").addEventListener("click", () => {
+  let url = urlInput.value.trim();
+  if (!url.startsWith("https://") && !url.startsWith("http://")) {
+    url = `https://${url}`;
+  } else if (url.startsWith("http://")) {
+    url = `https://${url.substring(7)}`;
   }
- 
+
   const win = window.open();
   win.document.body.style.margin = "0";
   win.document.body.style.height = "100vh";
@@ -82,12 +76,11 @@ document.getElementById("create").onclick = function () {
   iframe.style.border = "none";
   iframe.style.width = "100%";
   iframe.style.height = "100%";
-  iframe.src = urlInput.value;
+  iframe.src = url;
   iframe.referrerpolicy = "no-referrer";
   iframe.allow = "fullscreen";
   win.document.body.appendChild(iframe);
-};
-
+});
 
 // Event handler for the preset select dropdown
 presetSelect.addEventListener('change', function() {
@@ -106,7 +99,6 @@ presetSelect.addEventListener('change', function() {
   }
 });
 
-
 // Event listeners for the buttons
 document.getElementById('set-title-btn').addEventListener('click', changeTabTitle);
 document.getElementById('set-icon-btn').addEventListener('click', changeTabIcon);
@@ -114,6 +106,3 @@ document.getElementById('reset-btn').addEventListener('click', resetTabSettings)
 document.getElementById('imitate-canvas-btn').addEventListener('click', () => {
   applyUrl('https://wascouhsd.instructure.com/favicon.ico', 'Canvas');
 });
-
-
-
