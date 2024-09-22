3d30a28946fc34034282aa9c2344078020fdc2ee
Ezra-531 <4josephhernandez12@gmail.com>
2024-09-21 18:16:28 -0700

    bro my brother sucks at guitar

--- js/analytics.js
+++ js/analytics.js
@@ -1,16 +1,18 @@
 // analytics.js
 (function() {
-    var gtagScript = document.createElement('script');
-    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-BFKBY6VQ33';
-    gtagScript.async = true;
-    document.head.appendChild(gtagScript);
-  
-    var inlineScript = document.createElement('script');
-    inlineScript.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-BFKBY6VQ33');
-    `;
-    document.head.appendChild(inlineScript);
-  })();
+  if (window.gtag) return; // Exit if gtag is already defined
+
+  var gtagScript = document.createElement('script');
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-BFKBY6VQ33';
+  gtagScript.async = true;
+  document.head.appendChild(gtagScript);
+
+  var inlineScript = document.createElement('script');
+  inlineScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-BFKBY6VQ33');
+  `;
+  document.head.appendChild(inlineScript);
+})();
