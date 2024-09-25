a2b1a5b511549e3c64372742bd7f50fe2224432f
Ezra-531 <4josephhernandez12@gmail.com>
2024-09-24 19:46:24 -0700

    this def aint working but alr

---
+++ netlify/functions/auth.js
@@ -0,0 +1,26 @@
+exports.handler = async (event, context) => {
+  const VALID_CODE = '!@4002naoiR';
+
+  // Only handle POST requests
+  if (event.httpMethod === 'POST') {
+    const body = JSON.parse(event.body); // Parse the incoming request body
+
+    if (body.code === VALID_CODE) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Access granted' }),
+      };
+    } else {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({ message: 'try again lil bro' }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 405,
+      headers: { Allow: 'POST' },
+      body: `Method ${event.httpMethod} Not Allowed`,
+    };
+  }
+};
